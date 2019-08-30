a0d7c9464d623322357a3c550739c0e413696d7e
Cris Toozs <cris.hobson@hotmail.com>
2019-08-30 19:36:29 +0200

    remove optional part

--- index.js
+++ index.js
@@ -26,17 +26,14 @@ async function app() {
   document.getElementById('class-a').addEventListener('click', () => addExample(0));
   document.getElementById('class-b').addEventListener('click', () => addExample(1));
   document.getElementById('class-c').addEventListener('click', () => addExample(2));
-  addExample(3)
   while (true) {
     if (classifier.getNumClasses() > 0) {
       // Get the activation from mobilenet from the webcam.
       const activation = net.infer(webcamElement, 'conv_preds');
       // Get the most likely class and confidences from the classifier module.
       const result = await classifier.predictClass(activation);
- 	  if (result.classIndex == 3) {
- 	  	addExample(3)
- 	  }
-      const classes = ['A', 'B', 'C', 'None'];
+ 	 
+      const classes = ['A', 'B', 'C'];
       document.getElementById('console').innerText = `
         prediction: ${classes[result.classIndex]}\n
         probability: ${result.confidences[result.classIndex]}
